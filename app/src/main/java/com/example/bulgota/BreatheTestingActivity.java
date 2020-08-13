c220c899fd566e75177f9a6177ca188a758c210f
pje8845 <je991025@gmail.com>
2020-08-14 02:33:23 +0900

    Set Colors(primaryDark, Accent), BreatheTestingActivity UI완료

---
+++ app/src/main/java/com/example/bulgota/BreatheTestingActivity.java
@@ -0,0 +1,40 @@
+package com.example.bulgota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
+public class BreathTestingActivity extends AppCompatActivity {
+
+    public static LottieAnimationView lottieBreathTesting;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breath_testing);
+
+//        TextView tvTestingTitle = findViewById(R.id.tv_testing_title);
+//        TextView tvTestingInfo = findViewById(R.id.tv_breathe_testing_info);
+        LottieAnimationView lottieTesting = findViewById(R.id.lottie_breathe_testing);
+        setUpAnimation(lottieTesting);
+
+//        Typeface typeface_regular=Typeface.createFromAsset(getAssets(), "nanum_square_round_regular.ttf");
+//        Typeface typeface_bold=Typeface.createFromAsset(getAssets(), "nanum_square_round_bold.ttf");
+//        tvTestingTitle.setTypeface(typeface_bold);
+//        tvTestingInfo.setTypeface(typeface_regular);
+    }
+
+    private void setUpAnimation(LottieAnimationView animview) {
+        //재생할 애니메이션
+        animview.setAnimation("lottie_breathe_testing.json");
+        //반복횟수 지정 : 무한
+        animview.setRepeatCount(LottieDrawable.INFINITE); //아니면 횟수 지정
+        //시작
+        animview.playAnimation();
+
+    }
+}
