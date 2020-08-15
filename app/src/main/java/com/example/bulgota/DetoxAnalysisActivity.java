e557e975931a4794fa98271a1790c75b4df716c9
An-jonghun <dkswhdgnsvv@gmail.com>
2020-08-16 06:45:06 +0900

    .java
    layout 객체 설정
    
    그래프 custom 완료

--- app/src/main/java/com/example/bulgota/DetoxAnalysisActivity.java
+++ app/src/main/java/com/example/bulgota/DetoxAnalysisActivity.java
@@ -3,14 +3,17 @@ package com.example.bulgota;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -22,27 +25,67 @@ import com.github.mikephil.charting.data.LineDataSet;
 public class DetoxAnalysisActivity extends AppCompatActivity {
 
     private LineChart lineChart;
-    ArrayList<Entry> entry_chart = new ArrayList<>();
-    //차트
-
+    ArrayList<Entry> entry_chart;
+    //차트 객체
     private TextView countTime;
     MyTimer myTimer;    //타이머객체
-    //타이머
+
+    LinearLayout llNoticeView;  //상단 전체 layout
+    LinearLayout llState;   //주행가능 여부
+
+    RelativeLayout rlAlarm; //주행가능시 알람받기 layout
+
+    TextView tvAlcholLevel; //현재 알콜 농도
+    TextView tvMystate; //주행 가능 or 불가 텍스트
+    TextView tvtimer;   //주행가능까지 남은시간
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detox_analysis);
 
-        //카운트다운 선언
-        countTime = findViewById(R.id.tv_timer);
-        myTimer = new MyTimer(60000, 1000);
+        countTime = findViewById(R.id.tv_timer);    //타이머
+        lineChart = findViewById(R.id.chart);   //그래프
+
+        llNoticeView = findViewById(R.id.ll_notice_view);
+        llState = findViewById(R.id.ll_state);
+
+        rlAlarm = findViewById(R.id.rl_alarm);
+
+        tvAlcholLevel = findViewById(R.id.tv_alchol_level);
+        tvMystate = findViewById(R.id.tv_my_state);
+        tvtimer = findViewById(R.id.tv_timer);
+
+
+        rlAlarm.setOnClickListener(new RelativeLayout.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetoxAnalysisActivity.this, "알람받기 기능 구현 준비중", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myTimer = new MyTimer(600000, 1000);
         myTimer.start();
+        //카운트다운 선언
 
-        // 그래프선언
-        lineChart = findViewById(R.id.chart); //layout id
         LineData chartData = new LineData();
+        // 그래프선언
+        entry_chart = new ArrayList<>();
 
+        graphDataAdd(entry_chart);
+        //그래프에 들어갈 ArrayList 자료구조 데이터 추가 메서드
+
+        LineDataSet lineDataSet = new LineDataSet(entry_chart, "나");
+
+        chartSetting(lineChart,lineDataSet,chartData);
+        //차트 설정값 세팅 메서드
+
+    }
+
+    void graphDataAdd(ArrayList<Entry> entry_chart)
+    {
         Entry data1 = new Entry(15, 100);
         Entry data2 = new Entry(16, 90);
         Entry data3 = new Entry(17, 65);
@@ -54,48 +97,53 @@ public class DetoxAnalysisActivity extends AppCompatActivity {
         entry_chart.add(data3);   //x y 좌표
         entry_chart.add(data4);   //x y 좌표
         entry_chart.add(data5);   //x y 좌표
+    }
 
-        LineDataSet lineDataSet = new LineDataSet(entry_chart, "나");
+    void chartSetting(LineChart lineChart,LineDataSet lineDataSet, LineData chartData)
+    {
         lineDataSet.setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)); //LineChart에서 Line Color 설정
         lineDataSet.setCircleColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)); // LineChart에서 Line Circle Color 설정
         lineDataSet.setCircleHoleColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)); // LineChart에서 Line Hole Circle Color 설정
 
         chartData.addDataSet(lineDataSet);
 
-//        LineData lineData = new LineData(); //LineDataSet을 담는 그릇 여러개의 라인 데이터가 들어갈 수 있습니다.
-//        lineData.addDataSet(lineDataSet);
-//
-//        lineData.setValueTextColor(ContextCompat.getColor(getContext(), R.color.textColor)); //라인 데이터의 텍스트 컬러 설정
-//        lineData.setValueTextSize(9);
-//
-//        XAxis xAxis = lineChart.getXAxis(); // x 축 설정
-//        xAxis.setPosition(XAxis.XAxisPosition.TOP); //x 축 표시에 대한 위치 설정
-//        xAxis.setValueFormatter(new ChartXValueFormatter()); //X축의 데이터를 제 가공함. new ChartXValueFormatter은 Custom한 소스
-//        xAxis.setLabelCount(5, true); //X축의 데이터를 최대 몇개 까지 나타낼지에 대한 설정 5개 force가 true 이면 반드시 보여줌
-//        xAxis.setTextColor(ContextCompat.getColor(getContext(), R.color.textColor)); // X축 텍스트컬러설정
-//        xAxis.setGridColor(ContextCompat.getColor(getContext(), R.color.textColor)); // X축 줄의 컬러 설정
-//
-//        YAxis yAxisLeft = lineChart.getAxisLeft(); //Y축의 왼쪽면 설정
-//        yAxisLeft.setTextColor(ContextCompat.getColor(getContext(), R.color.textColor)); //Y축 텍스트 컬러 설정
-//        yAxisLeft.setGridColor(ContextCompat.getColor(getContext(), R.color.textColor)); // Y축 줄의 컬러 설정
-//
-//        YAxis yAxisRight = lineChart.getAxisRight(); //Y축의 오른쪽면 설정
-//        yAxisRight.setDrawLabels(false);
-//        yAxisRight.setDrawAxisLine(false);
-//        yAxisRight.setDrawGridLines(false);
-//        //y축의 활성화를 제거함
-//
-//        lineChart.setVisibleXRangeMinimum(60 * 60 * 24 * 1000 * 5); //라인차트에서 최대로 보여질 X축의 데이터 설정
-//        lineChart.setDescription(null); //차트에서 Description 설정 저는 따로 안했습니다.
-//
-//        Legend legend = lineChart.getLegend(); //레전드 설정 (차트 밑에 색과 라벨을 나타내는 설정)
-//        legend.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);//하단 왼쪽에 설정
-//        legend.setTextColor(ContextCompat.getColor(getContext(), R.color.textColor)); // 레전드 컬러 설정
-       // lineChart.setData(lineData);
+        chartData.setValueTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); //라인 데이터의 텍스트 컬러 설정
+        chartData.setValueTextSize(11);
+
+        XAxis xAxis = lineChart.getXAxis(); // x 축 설정
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //x 축 표시에 대한 위치 설정
+        xAxis.setLabelCount(5, true); //X축의 데이터를 최대 몇개 까지 나타낼지에 대한 설정 5개 force가 true 이면 반드시 보여줌
+        xAxis.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); // X축 텍스트컬러설정
+        xAxis.setGridColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); // X축 줄의 컬러 설정
+
+
+        YAxis yAxisLeft = lineChart.getAxisLeft(); //Y축의 왼쪽면 설정
+        yAxisLeft.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); //Y축 텍스트 컬러 설정
+        yAxisLeft.setGridColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent)); // Y축 줄의 컬러 설정
+
+        YAxis yAxisRight = lineChart.getAxisRight(); //Y축의 오른쪽면 설정
+        yAxisRight.setDrawLabels(false);
+        yAxisRight.setDrawAxisLine(false);
+        yAxisRight.setDrawGridLines(false);
+        //y축의 활성화를 제거함
+
+        Description description = new Description();
+        description.setTextSize(12);
+        description.setText("predicted by Withmark.");
+        lineChart.setDescription(description); //오른쪽 하단에 description 설정
 
         lineChart.setData(chartData);
         lineChart.invalidate();
 
+        Legend legend = lineChart.getLegend(); //레전드 설정 (차트 밑에 색과 라벨을 나타내는 설정)
+        legend.setDirection(Legend.LegendDirection.RIGHT_TO_LEFT);//색과 라벨 위치설정
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL); // 라벨 그래프와 수평
+        legend.setFormSize(10); //차트 색 크기
+        legend.setTextSize(9); //라벨 text 크기
+        legend.setForm(Legend.LegendForm.CIRCLE);   //차트 형태
+        legend.setYOffset(20);  //라벨과 그래프 offset
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);   //라벨 위치
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);    //라벨 위치
     }
 
     class MyTimer extends CountDownTimer
