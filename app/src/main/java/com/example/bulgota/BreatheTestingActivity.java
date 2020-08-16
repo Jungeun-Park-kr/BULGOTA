dd96c892055f0c5041bbb5f132073ea41b69d77d
yeonghyeonSeok <syh4834@naver.com>
2020-08-17 01:45:56 +0900

    auto bluetooth

--- app/src/main/java/com/example/bulgota/BreatheTestingActivity.java
+++ app/src/main/java/com/example/bulgota/BreatheTestingActivity.java
@@ -1,11 +1,18 @@
 package com.example.bulgota;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.animation.Animator;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,124 +21,184 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieDrawable;
 
-import app.akexorcist.bluetotohspp.library.BluetoothSPP;
-import app.akexorcist.bluetotohspp.library.BluetoothState;
-import app.akexorcist.bluetotohspp.library.DeviceList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 
 public class BreatheTestingActivity extends AppCompatActivity {
+    TextView tvValue;
+    Button btnMeasure;//측정하기 화면 이동 버튼
 
-    private BluetoothSPP btSpp; //블루투스 통신
-    Button btnMeasureActivity;//측정하기 화면 이동 버튼
+    BluetoothAdapter bluetoothAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    List<String> listPairedDevices;
+
+    Handler bluetoothHandler;
+    ConnectedBluetoothThread threadConnectedBluetooth;
+    BluetoothDevice bluetoothDevice;
+    BluetoothSocket bluetoothSocket;
+
+    final static int BT_REQUEST_ENABLE = 1;
+    final static int BT_MESSAGE_READ = 2;
+    final static int BT_CONNECTING_STATUS = 3;
+    final static UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     public static LottieAnimationView lottieBreathTesting; //(로딩모양)측정중 로띠띠
    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_breathe_testing);
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_breathe_testing);
+       LottieAnimationView lottieTesting = findViewById(R.id.lottie_breathe_testing);
+       setUpAnimation(lottieTesting);
 
-        LottieAnimationView lottieTesting = findViewById(R.id.lottie_breathe_testing);
-        setUpAnimation(lottieTesting); //애니메이션 등록
+       tvValue = findViewById(R.id.tv_value);
+       btnMeasure = findViewById(R.id.btn_measure);
 
-    /////여기서부터 블루투스 ///////
-       btSpp = new BluetoothSPP(this); //초기화
+       bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-       if (!btSpp.isBluetoothAvailable()) { //블루투스 사용 불가능할 경우
-           Toast.makeText(getApplicationContext()
-                   , "Bluetooth is not available"
-                   , Toast.LENGTH_SHORT).show();
-           finish();
-       }
-
-       btSpp.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신하기
-           /* 아두이노에서 넘어온 데이터 수신 : 아두이노는 1byte씩 데이터 수신됨.
-            *   따라서 아두이노에서 온 데이터를 넣어 바이트를 모두 합친 후 msg를 통해 return됨
-            *   즉 우리가 사용할 것 : msg!! // 아래는 msg를 토스트로 띄운 예시
-            */
-           TextView tvValue = findViewById(R.id.tv_value);
-
-           @Override
-           public void onDataReceived(byte[] data, String msg) { //데이터 수신 받을 때
-               int iValue = Integer.parseInt(msg);
-               tvValue.setText("측정값:" + iValue);
-           }
+       btnMeasure.setOnClickListener(l -> {
+           listPairedDevices();
        });
 
-       btSpp.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
-           @Override
-           public void onDeviceConnected(String name, String address) {
-               Toast.makeText(getApplicationContext()
-                       , "Connected to " + name + "\n" + address
-                       , Toast.LENGTH_SHORT).show();
-           }
-
-           @Override
-           public void onDeviceDisconnected() { //연결 해제
-               Toast.makeText(getApplicationContext()
-                       , "Connection lost", Toast.LENGTH_SHORT).show();
-           }
-
-           @Override
-           public void onDeviceConnectionFailed() { //연결 실패
-               Toast.makeText(getApplicationContext()
-                       , "Unable to connect", Toast.LENGTH_SHORT).show();
-           }
-       });
-
-       ////////////////////////////////////////////////////////////////연결 시도
-       Button btnConnect = findViewById(R.id.btn_measure); //측정하기 버튼
-       btnConnect.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if (btSpp.getServiceState() == BluetoothState.STATE_CONNECTED) {
-                   btSpp.disconnect();
-               } else {
-                   Intent intent = new Intent(getApplicationContext(), DeviceList.class);
-                   startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+       bluetoothHandler = new Handler(){
+           public void handleMessage(android.os.Message msg){
+               if(msg.what == BT_MESSAGE_READ){
+                   String readMessage = null;
+                   try {
+                       readMessage = new String((byte[]) msg.obj, "UTF-8");
+                   } catch (UnsupportedEncodingException e) {
+                       e.printStackTrace();
+                   }
+                   tvValue.setText(readMessage);
                }
+               lottieTesting.cancelAnimation();
            }
-       });
+       };
+
+
+//       LottieAnimationView lottieTesting = findViewById(R.id.lottie_breathe_testing);
+//       setUpAnimation(lottieTesting); //애니메이션 등록
+   }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case BT_REQUEST_ENABLE:
+                if (resultCode == RESULT_OK) { // 블루투스 활성화를 확인을 클릭하였다면
+                    Toast.makeText(getApplicationContext(), "블루투스 활성화", Toast.LENGTH_LONG).show();
+                } else if (resultCode == RESULT_CANCELED) { // 블루투스 활성화를 취소를 클릭하였다면
+                    Toast.makeText(getApplicationContext(), "취소", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        btSpp.stopService(); //블루투스 중지
-    }
+    void listPairedDevices() {
+        if (bluetoothAdapter.isEnabled()) {
+            pairedDevices = bluetoothAdapter.getBondedDevices();
 
-    public void onStart() {
-        super.onStart();
-        if (!btSpp.isBluetoothEnabled()) {
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
-        } else {
-            if (!btSpp.isServiceAvailable()) {
-                btSpp.setupService();
-                btSpp.startService(BluetoothState.DEVICE_OTHER); //아두이노와 연결 / DEVICE_ANDROID는 안드로이드 기기 끼리
+            if (pairedDevices.size() > 0) {
 
+                listPairedDevices = new ArrayList<String>();
+                for (BluetoothDevice device : pairedDevices) {
+                    listPairedDevices.add(device.getName());
+                }
+                final CharSequence[] items = listPairedDevices.toArray(new CharSequence[listPairedDevices.size()]);
+                listPairedDevices.toArray(new CharSequence[listPairedDevices.size()]);
+
+                connectSelectedDevice("MINCHO");
+
+            } else {
+                Toast.makeText(getApplicationContext(), "페어링된 장치가 없습니다.", Toast.LENGTH_LONG).show();
             }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "블루투스가 비활성화 되어 있습니다.", Toast.LENGTH_SHORT).show();
         }
     }
 
+    void connectSelectedDevice(String selectedDeviceName) {
+        for(BluetoothDevice tempDevice : pairedDevices) {
+            if (selectedDeviceName.equals(tempDevice.getName())) {
+                Log.e("click3", tempDevice.getName());
+                bluetoothDevice = tempDevice;
+                break;
+            }
+        }
+        try {
 
-    public void measure() {
-        Button btnMeasure = findViewById(R.id.btn_measure); //
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(BT_UUID);
+            bluetoothSocket.connect();
+            threadConnectedBluetooth = new ConnectedBluetoothThread(bluetoothSocket);
+            threadConnectedBluetooth.start();
+            bluetoothHandler.obtainMessage(BT_CONNECTING_STATUS, 1, -1).sendToTarget();
+            Log.e("click4", String.valueOf(bluetoothSocket));
+
+//            if(tvValue != null) {
+//                Log.e("result", "mincho");
+//                lottieBreathTesting.cancelAnimation();
+//
+//            }
+
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "블루투스 연결 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+        }
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK)
-                btSpp.connect(data);
-        } else if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK) {
-                btSpp.setupService();
-                btSpp.startService(BluetoothState.DEVICE_OTHER);
-                // setup();
-            } else {
-                Toast.makeText(getApplicationContext()
-                        , "Bluetooth was not enabled."
-                        , Toast.LENGTH_SHORT).show();
-                finish();
+    private class ConnectedBluetoothThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedBluetoothThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 연결 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+            Log.e("bluetooth", "연결");
+
+        }
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.available();
+                    if (bytes != 0) {
+                        SystemClock.sleep(100);
+                        bytes = mmInStream.available();
+                        bytes = mmInStream.read(buffer, 0, bytes);
+                        bluetoothHandler.obtainMessage(BT_MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                        Log.e("bluetooth2", "연결");
+
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), "소켓 해제 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -143,6 +210,29 @@ public class BreatheTestingActivity extends AppCompatActivity {
         animview.setRepeatCount(LottieDrawable.INFINITE); //아니면 횟수 지정
         //시작
         animview.playAnimation();
+
+        animview.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                animview.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
 
     }
 }
