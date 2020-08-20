7d449d83cbd346429e97e9c21bcaf606f7c5103f
pje8845 <je991025@gmail.com>
2020-08-20 14:30:40 +0900

    Real) get permissions on DeviceMapActivity

--- app/src/main/java/com/example/bulgota/MainActivity.java
+++ app/src/main/java/com/example/bulgota/MainActivity.java
@@ -1,29 +1,14 @@
 package com.example.bulgota;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //퍼미션 리스트
-    private static String[] permission_list = {
-            Manifest.permission.INTERNET,
-            Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.CAMERA
-    };
-
     Button deviceMapBtn;
     Button breathTestingBtn;
     Button detoxAnalysisBtn;
@@ -35,8 +20,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        checkPermission();
 
         deviceMapBtn = findViewById(R.id.btn_device_map);
         breathTestingBtn = findViewById(R.id.btn_breath_testing);
@@ -86,34 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
 
+
+
     }
-
-    public void checkPermission(){
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) //안드로이드 버전 6.0 미만일시 리턴
-            return;
-
-        for (String permission : permission_list) { //권한 허용 여부 확인
-            int check = checkCallingOrSelfPermission(permission);
-
-            if (check == PackageManager.PERMISSION_DENIED) //권한 허용여부 확인 창 띄우기
-                requestPermissions(permission_list,0);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == 0) { //퍼미션 리스트 하나씩 확인
-            for (int i = 0; i < grantResults.length; i++) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) //허용 되었다면
-                    ; //허용 되었음 동작할 메소드
-                else { //허용 되지 않은 경우 앱종료
-                    Toast.makeText(this, "불고타 서비스 이용을 위해 권한이 필요합니다.", Toast.LENGTH_LONG).show();
-                    //showRequestAgainDialog();
-                }
-            }
-        }
-    }
-
 }
